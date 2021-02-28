48808a8fbf62d11d5af66e5d1a0e73ebb979e4c9
Darren Gussin <dgussin@msn.com>
2021-02-28 10:46:01 -0500

    video 154 - Mongoose Pre-Save Hooks

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
 
 // an interface that describes the properties
 // that are requierd to create a new User
@@ -9,8 +11,8 @@ interface UserAttrs {
 
 // an interface that describes the properties
 // that a User Model has
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
 }
 
 // An interface that describes the properties
@@ -31,11 +33,18 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.pre('save', async function(done) {
+  if(this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
-
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
