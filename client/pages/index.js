25df41912efc86a308bb6aae42f8ccd49116e65a
Darren Gussin <dgussin@msn.com>
2021-08-24 23:15:54 -0400

    video 202 - 232 -Passing Through the Cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,32 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+  console.log(currentUser);
   return <h1>Landing Page</h1>
-}
+};
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if(typeof window === 'undefined') {
+    // are on the server
+    // requests should be made to http://ingress-nginx.ingress-nginx.srv.xxxxxx
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser
+    // request can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
