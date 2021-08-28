87c2f6f8f753b72b6cb5723073a2adc097006566
Darren Gussin <dgussin@msn.com>
2021-08-28 16:31:08 -0400

    video 236 - A Reusable Header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,7 +3,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 const main = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default main;
