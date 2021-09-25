eb36ebe8f28aa2a2704033a6eecada1f2d481bda
Darren Gussin <dgussin@msn.com>
2021-09-25 09:45:04 -0400

    video 256 - Project Setup - tickets

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@dlgtickets/common';
+
+const app = express();
+
+// since traffic if coming from nginx proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
